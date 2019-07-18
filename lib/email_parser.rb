a49f052af3b97075aa023d6695f3da011fd15cae
Valena Del Valle <valenadelvalle4@gmail.com>
2019-07-18 15:17:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,8 +2,7 @@ class EmailParser
   attr_accessor :emails
   
   def parse
-    emails = ["",""]
-    parser = EmailParser.new("avi@test.com, arel@test.com")
-    EmailParser.parse
+    
   end
+  
 end
